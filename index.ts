b7bfe6c7fcec9bcd4436b0c3e471fc7d1fc997d9
channayousif <channayousif@yahoo.com>
2024-03-25 03:07:39 +0500

    patch

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-#! usr/dev/bin node
+#! /usr/bin/env node
 import inquirer from "inquirer";
-
+console.log("\n\nSimple CLI Calculator - by Channayousif\n\n")
 const input = await inquirer.prompt([
   { message: "Enter first number", type: "number", name: "Num1" },
   { message: "Enter 2nd number", type: "number", name: "Num2" },
@@ -11,6 +11,7 @@ const input = await inquirer.prompt([
     choices: ["addition", "substraction", "multiplication", "division","square of first num"],
   }
 ]);
+console.log("\n\tR E S U L T\n")
 console.log(input)
 if (input.operation=="addition"){
     console.log(input.Num1+input.Num2)
@@ -27,3 +28,4 @@ else if (input.operation=="division"){
 else if (input.operation=="square of first num"){
     console.log(input.Num1 * input.Num1)
 }
+console.log("Thanks for using my calculator!")
